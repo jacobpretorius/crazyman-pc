2ff79e23878a193db09731761cb796121c3f28a5
Jacob Pretorius <jacob@jcpretorius.com>
2019-10-24 20:45:30 +0100

    Add comments on work that is outstanding

--- src/components/ALU/ALUOperations.js
+++ src/components/ALU/ALUOperations.js
@@ -1,5 +1,9 @@
 import { base10ToBoolArray, boolArrayToBase10 } from '../../utils/BusConversions.js';
 
+// I know, I know. These aren't actually bitwise additions/subtractions. They are done in
+// plain old base 10. I'll get round to changing them sometime.
+// These operations being bitwise are less important to me than having the Ben Eater CPU running. MVP.
+
 function boolArrayAddition(arr1, arr2) {
   var arr1AsBase10 = boolArrayToBase10(arr1);
   var arr2AsBase10 = boolArrayToBase10(arr2);
